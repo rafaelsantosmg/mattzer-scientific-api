6ca2ea4c936151deff38ee8c59f2586ef57d1e3b
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-09-01 11:03:01 -0300

    feat(API): Implementada a configuração inicial da api

---
+++ backend/src/api.js
@@ -0,0 +1,17 @@
+require('express-async-errors');
+const express = require('express');
+const cors = require('cors');
+const handleError = require('./middlewares/handleError');
+const router = require('./routers');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(router);
+
+app.use(handleError);
+
+module.exports = app;
